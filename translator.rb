89d0a16c688c54d671d059563ca92cc680d25eca
Michael Sprauer <Michael@Sprauer.net>
2016-01-31 18:45:00 +0100

    initial

---
+++ translator.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+# Required software:
+####################
+# 
+# sudo apt-get install qrencode icecast2 rbenv
+# rbenv install 2.2.3 && rbenv global 2.2.3
+#
+
+require 'webrick'
+
+http_port = 5000
+host_ip = `dig +short \`hostname\``.chomp
+shout_url = "http://#{host_ip}:8000"
+web_url = "http://#{host_ip}:#{http_port}"
+stream_name = "stream"
+stream_url = "#{shout_url}/#{stream_name}.mp3"
+
+puts "Update qrcode"
+`qrencode -o stream_qrc.png -s 5 '#{stream_url}'`
+
+puts "Update index.html"
+filename="index.html"
+outdata = File.read(filename).gsub(/<a(href=.*)?>.*<\/a>/, "<a href=#{stream_url.inspect}>#{stream_url}</a>")
+
+File.open(filename, 'w') do |out|
+  out << outdata
+end  
+
+
+puts "Start Icecast2 (su required)"
+`sudo service icecast2 start`
+
+
+puts "Stargin Webserver on #{web_url}"
+server = WEBrick::HTTPServer.new Port: http_port, BindAddress: host_ip
+trap 'INT' do 
+    begin 
+        server.shutdown
+    rescue Exception => e
+        puts e
+        exit 1
+    end
+end
+Thread.new do
+    server.start
+end
+
+transcoder = "#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100}"
+shout_sink = "{access=shout,mux=mp3,dst=source:hackme@localhost:8000/#{stream_name}.mp3}\""
+cmd = "cvlc alsa://default :live-caching=500 --sout-shout-mp3 :sout=\"#{transcoder}:std#{shout_sink}"
+puts "Start Streaming"
+puts `#{cmd}`
+
+puts "Shutting down"
+server.shutdown
